0135a338501ded61d0c04130ba7d0eb53025208e
Olivier Petit <petit-olivier@hotmail.fr>
2018-01-23 09:40:40 +0100

    Better ws2812 timing strategy

--- FPGA/ws2812b_strip_a1.vhd
+++ FPGA/ws2812b_strip_a1.vhd
@@ -22,47 +22,47 @@ architecture a1 of ws2812b_strip is
 	
 	signal bit_number : integer range 0 to 23; -- Bit currently being tranmited
 	signal led_number : integer range 0 to N - 1; -- Led being transmited
-	signal clock_10 : std_logic; -- 10Mhz clock
-	signal clock_counter : unsigned(2 downto 0); -- Counter to generate clock
-	signal bit_counter : unsigned(9 downto 0); -- For each bit, will count from 0 to 12 to generate the appropriate output
+	signal clock_2_5 : std_logic; -- 10Mhz clock
+	signal clock_counter : unsigned(4 downto 0); -- Counter to generate clock
+	signal bit_counter : unsigned(1 downto 0); -- For each bit, will count from 0 to 3 to generate the appropriate output
 	signal reset_mode : std_logic; -- Flag for strip reset state
 	
 	begin
 	
-	-- Generates 10Mhz clock
+	-- Generates 2.5Mhz clock
 	process(RESET, CLOCK)
 	begin
 		if RESET = '0' then
 			clock_counter <= (others => '0');
 		elsif CLOCK'event then
-			if clock_counter = "101" then
+			if clock_counter = "10100" then
 				clock_counter <= (others => '0');
-				clock_10 <= not clock_10;
+				clock_2_5 <= not clock_2_5;
 			else
 				clock_counter <= clock_counter + 1;
 			end if;
 		end if;
 	end process;
 	
-	-- One bit = 1.2us = 12 clock cycles
-	-- Reset = 60 us = 600 clock cycles
-	process(RESET, clock_10)
+	-- One bit = 1.2us = 3 clock cycles
+	-- Reset = 60 us = 150 clock cycles
+	process(RESET, clock_2_5)
 	begin
 		if RESET = '0' then
 			bit_counter <= (others => '0');
 			reset_mode <= '1';
 			bit_number <= 0;
 			led_number <= 0;
-		elsif rising_edge(clock_10) then
+		elsif rising_edge(clock_2_5) then
 			if reset_mode = '1' then
-				if bit_counter = "1001011000" then -- Count 0 to 601
+				if bit_counter = "10010110" then -- Count 0 to 150
 					reset_mode <= '0';
 					bit_counter <= (others => '0');
 				else
 					bit_counter <= bit_counter + 1;
 				end if;
 			else -- transmiting
-				if bit_counter = "0000001011" then -- Count 0 to 11
+				if bit_counter = "11" then -- Count 0 to 3
 					bit_counter <= (others => '0');
 					
 					if bit_number = 23 then
@@ -97,13 +97,13 @@ architecture a1 of ws2812b_strip is
 			end if;
 			
 			if bit_to_transmit = '0' then
-				if to_integer(bit_counter) < 4 then
+				if to_integer(bit_counter) < 1 then
 					OUTPUT <= '1';
 				else
 					OUTPUT <= '0';
 				end if;
 			else
-				if to_integer(bit_counter) < 8 then
+				if to_integer(bit_counter) < 2 then
 					OUTPUT <= '1';
 				else
 					OUTPUT <= '0';
